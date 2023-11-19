bce5e93c3eb4f716290ecb9ae7d6d239bdc8e102
d.rodkin <metastasis@protonmail.ch>
2023-11-20 00:29:46 +0300

    feat: wildberries product crawling

--- src/main.ts
+++ src/main.ts
@@ -14,52 +14,127 @@ import { Page } from "playwright";
 - Оценка товара
 - Количество оценок товара
 * */
-type WildberriesProduct = {
-  title: string,
+type Company = {
+  name: string,
+  type: 'ip' | 'ooo' | 'zao' | 'ao' | 'other' | null,
+  // company from breadcrumbs or from sidebar can be different
+  // company from sidebar can be used for marketing purposes
+  // and from breadcrumbs is more reliable
+  parsedFrom: 'fromBreadcrumbs' | 'fromSidebar',
   url: string,
-  price: string,
-  rating: string,
-  ratingCount: string,
-  company: string,
 };
-type WildberriesCrawlerResult = {
-  // products: WildberriesProduct[],
-  total: number,
+type Size = {
+  length?: number,
+  width?: number,
+  height?: number,
+};
+type CategoryTrait = {
   // top category, e.g. "Женщинам"
   category: string,
   // subcategory, e.g. "Блузки и рубашки"
   subcategory: string,
   // goods type, e.g. "Блузка-боди"
   goodsCategory: string,
+}
+type WildberriesCategoryPageResult = CategoryTrait & {
+  total: number,
+};
+type WildberriesProductPageResult = {
+  title: string,
+  // url: string,
+  price: number,
+  rating: number | null,
+  ratingCount: number | null,
+  company?: Company,
+  sizes?: Size,
 };
 
 const wildberriesSelectors = {
-  productsTotal: "span.goods-count",
+  categoryTitle: "h1.catalog-title",
   breadcrumbs: "ul.breadcrumbs__list",
   categoryFilters: ".dropdown-filter",
-  categoryFilterOpened: ".dropdown-filter .selected"
+  categoryFilterOpened: ".dropdown-filter .selected",
+  productLinks: ".product-card__link",
+} as const;
+const wildberriesProductSelectors = {
+  title: ".product-page__header > h1",
+  company: ".breadcrumbs__list li:last-child",
+  price: ".product-page__aside-container .price-block__final-price",
+  rating: ".product-page__common-info .product-review__rating",
+  ratingCount: ".product-page__common-info .product-review__count-review",
+};
+const labels = {
+  product: 'PRODUCT',
 } as const;
 
-async function getPageHtml(page: Page): Promise<WildberriesCrawlerResult> {
-  const productsTotal = await page.locator(wildberriesSelectors.productsTotal).innerText();
-  // const total = parseInt(productsTotal, 10);
+async function getPageHtml(page: Page): Promise<WildberriesCategoryPageResult> {
   const breadcrumbsRaw = await page.locator(wildberriesSelectors.breadcrumbs).innerText();
   const breadcrumbs = breadcrumbsRaw.split("\n").map((item) => item.trim().toLowerCase()).filter((item) => {
     return item.length > 0 && item !== "главная";
   });
   const [category, subcategory] = breadcrumbs;
-  const categoryFilters = await page.locator(wildberriesSelectors.categoryFilters).filter({hasText: 'Категория'});
-  const categoryButton = await categoryFilters.locator('button');
+  const categoryFilters = await page.locator(wildberriesSelectors.categoryFilters).filter({hasText: "Категория"});
+  const categoryButton = await categoryFilters.locator("button");
   await categoryButton.click();
   const categoryFilterOpened = await page.locator(wildberriesSelectors.categoryFilterOpened).last().textContent() || '';
   const categoryWithTotal = categoryFilterOpened.toLowerCase().trim();
-  const goodsCategory = categoryWithTotal.replace(/[^а-яА-Я\-]/g, '');
-  const total = parseInt(categoryWithTotal.replace(/[^\d]/g, ''), 10)
+  const goodsCategory = categoryWithTotal.replace(/[^а-яА-Я\-]/g, "");
+  const total = parseInt(categoryWithTotal.replace(/[^\d]/g, ''), 10);
   return {
     total,
     category,
     subcategory,
-    goodsCategory
+    goodsCategory,
+  };
+}
+
+async function scrapProduct(page: Page): Promise<WildberriesProductPageResult> {
+  const titleRaw = await page.locator(wildberriesProductSelectors.title).textContent();
+  if (!titleRaw) {
+    throw new Error(`Title not found on ${page.url()}`);
+  }
+  const title = titleRaw.trim().toLowerCase();
+  const priceRaw = await page.locator(wildberriesProductSelectors.price).textContent();
+  if (!priceRaw) {
+    throw new Error(`Price not found on ${page.url()}`);
+  }
+  const price = parseFloat(priceRaw.replace(/[^\d]/g, ''));
+  const companyLocator = await page.locator(wildberriesProductSelectors.company);
+  const companyTitleRaw = await companyLocator.textContent();
+  if (!companyTitleRaw) {
+    throw new Error(`Company title not found on ${page.url()}`);
+  }
+  const companyTitle = companyTitleRaw.trim().toLowerCase();
+  const companyLinkRaw = await companyLocator.locator('a').getAttribute('href');
+  if (!companyLinkRaw) {
+    throw new Error(`Company link not found on ${page.url()}`);
+  }
+  const baseUrl = new URL(page.url()).origin;
+  const companyLink = `${baseUrl}${companyLinkRaw}`;
+  const company: Company = {
+    name: companyTitle,
+    type: null,
+    url: companyLink,
+    parsedFrom: 'fromBreadcrumbs',
+  };
+  const ratingRaw = await page.locator(wildberriesProductSelectors.rating).textContent();
+  if (!ratingRaw) {
+    throw new Error(`Rating not found on ${page.url()}`);
+  }
+  // There may be no ratings.
+  // If there are none, then the text “No ratings” will appear.
+  const rating = parseFloat(ratingRaw.replace(/[^\d]/g, '')) || null;
+  const ratingCountRaw = await page.locator(wildberriesProductSelectors.ratingCount).textContent();
+  if (!ratingCountRaw) {
+    throw new Error(`Rating count not found on ${page.url()}`);
+  }
+  const ratingCount = parseInt(ratingCountRaw.replace(/[^\d]/g, ''), 10) || null;
+  return {
+    title,
+    price,
+    company,
+    rating,
+    ratingCount,
   };
 }
 
@@ -69,7 +144,6 @@ if (process.env.NO_CRAWL !== "true") {
   const crawler = new PlaywrightCrawler({
     // Use the requestHandler to process each of the crawled pages.
     async requestHandler({ request, page, enqueueLinks, log, pushData }) {
-
       if (config.cookie) {
         // Set the cookie for the specific URL
         const cookie = {
@@ -80,14 +154,31 @@ if (process.env.NO_CRAWL !== "true") {
         await page.context().addCookies([cookie]);
       }
 
-      const title = await page.title();
+      if (request.label === labels.product) {
+        log.info(`Crawling ${request.loadedUrl}, label is ${request.label}...`);
+        await page.waitForSelector(wildberriesProductSelectors.title, {
+          timeout: config.waitForSelectorTimeout ?? 1000,
+        });
+        const category: CategoryTrait = {
+          category: request.userData.category,
+          subcategory: request.userData.subcategory,
+          goodsCategory: request.userData.goodsCategory,
+        };
+        const data: WildberriesProductPageResult = await scrapProduct(page);
+        await pushData({ title: data.title, url: request.loadedUrl, data, category });
+        return;
+      }
+
+      const titleRaw = await page.locator(wildberriesSelectors.categoryTitle).textContent();
+      const title = titleRaw ? titleRaw.trim().toLowerCase() : null;
+
       log.info(`Crawling ${request.loadedUrl}...`);
 
       await page.waitForSelector(config.selector, {
         timeout: config.waitForSelectorTimeout ?? 1000,
       });
 
-      const data: WildberriesCrawlerResult = await getPageHtml(page);
+      const data: WildberriesCategoryPageResult = await getPageHtml(page);
 
       // Save results as JSON to ./storage/datasets/default
       await pushData({ title, url: request.loadedUrl, data });
@@ -99,8 +190,17 @@ if (process.env.NO_CRAWL !== "true") {
       // Extract links from the current page
       // and add them to the crawling queue.
       await enqueueLinks({
-        globs: [config.match],
+        selector: wildberriesSelectors.productLinks,
+        label: labels.product,
+        userData: {
+          category: data.category,
+          subcategory: data.subcategory,
+          goodsCategory: data.goodsCategory,
+        }
       });
+      // await enqueueLinks({
+      //   globs: [config.match],
+      // });
     },
     // Comment this option to scrape the full website.
     maxRequestsPerCrawl: config.maxPagesToCrawl,
@@ -109,9 +209,7 @@ if (process.env.NO_CRAWL !== "true") {
   });
 
   // Add first URL to the queue and start the crawl.
-  await crawler.run([config.url], {
-
-  });
+  await crawler.run([config.url]);
 }
 
 const jsonFiles = await glob("storage/datasets/default/*.json", {
